bb68af4b03a4bc03a188fb69a4d417f4e412a9ac
lgrodriguez <lauragarciarod@gmail.com>
2023-02-01 18:37:12 +0100

    add team controller, model & views for crud

---
+++ resources/views/teams/show.blade.php
@@ -0,0 +1,16 @@
+@extends('layouts.plantilla')
+
+@section('title',$team->name)
+
+@section('content')
+<h1>Club {{$team->name}}</h1>
+<p>{{$team->location}}</p>
+<a href="{{route('teams.edit', $team)}}">Editar este equipo</a> 
+<br>
+<form action="{{route('teams.destroy',$team)}}" method="POST">
+@csrf
+@method('delete')
+<button type="submit">Eliminar</button>
+</form>
+<a href="{{route('teams.index')}}">Volver a equipos</a> 
+@endsection
