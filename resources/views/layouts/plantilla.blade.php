592774a6ca135e7d31ee01111f48aadbc1dc416b
lgrodriguez <lauragarciarod@gmail.com>
2023-02-01 14:05:02 +0100

    create layout template with blade

---
+++ resources/views/layouts/plantilla.blade.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="utf-8">
+  <title>@yield('title')</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <!-- favicon -->
+  <!-- estilos -->
+</head>
+
+<body>
+  <!-- header -->
+  <!-- nav -->
+  
+  @yield ('content')
+
+  <!-- footer -->
+
+  <!-- script -->
+
+</body>
+</html>
