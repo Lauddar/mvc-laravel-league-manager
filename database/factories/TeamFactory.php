31135d174e25942a8bc581744757bb29c25f5a13
lgrodriguez <lauragarciarod@gmail.com>
2023-02-02 21:50:12 +0100

    add seeders

---
+++ database/factories/TeamFactory.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\Team;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Club>
+ */
+class TeamFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    protected $model = Team::class;
+
+    public function definition()
+    {
+        return [
+            'club_id'=>50,
+            'name' => $this->faker->sentence(3,false),
+            'category' => $this->faker->randomElement(['Junior', 'Infantil'])
+        ];
+    }
+}
