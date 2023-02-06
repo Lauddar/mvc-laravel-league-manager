724b50fc0619c981ab31849aa338b78dd45a8d7a
lgrodriguez <lauragarciarod@gmail.com>
2023-02-06 09:32:06 +0100

    modify database and create seeders

---
+++ database/migrations/2023_02_01_134717_create_league_team_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('league_team', function (Blueprint $table) {
+            $table->unsignedBigInteger('league_id');
+            $table->unsignedBigInteger('team_id');
+            $table->string('punctuation', 45)->nullable();
+            $table->string('position', 45)->nullable();
+            $table->string('victories', 45)->nullable();
+            $table->string('defeats', 45)->nullable();
+            $table->string('ties', 45)->nullable();
+            $table->string('scored_goals', 45)->nullable();
+            $table->string('conceded_goals', 45)->nullable();
+
+            $table->primary(['league_id', 'team_id']);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('league_has_teams');
+    }
+};
--- database/migrations/2023_02_01_134717_create_leagues_table.php
+++ database/migrations/2023_02_01_134717_create_leagues_table.php
@@ -16,6 +16,7 @@ return new class extends Migration
         Schema::create('leagues', function (Blueprint $table) {
             $table->id();
             $table->timestamps();
+            $table->string('name');
             $table->dateTime('start_date')->nullable();
             $table->dateTime('end_date')->nullable();
             $table->enum('state', ['TO START', 'IN PROGRESS', 'FINISHED'])->nullable();
