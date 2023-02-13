6c87fa10a815bf67f893e66e8fb1b9e2c097e1cc
lgrodriguez <lauragarciarod@gmail.com>
2023-02-13 10:01:23 +0100

    add clubs views

--- resources/views/clubs/index.blade.php
+++ resources/views/clubs/index.blade.php
@@ -2,13 +2,23 @@
 
 @section('title','Clubs')
 
+@section('pretitle', 'CLUBS')
+
 @section('content')
-<h1>Clubs</h1>
-<a href="{{route('clubs.create')}}">Crear club</a>
-<ul>
-    @foreach ($clubs as $club)
-    <li><a href="{{route('clubs.show', $club->id)}}">{{$club->name}}</a></li>
-    @endforeach
-</ul>
-{{$clubs->links()}}
+<div class="mt-5 flex space-x-20 font-rubik">
+    <div><a href="{{route('clubs.create')}}" class="w-28 h-11 m-8 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-neon-orange px-4 py-2 text-white shadow-sm hover:bg-dark-orange">Crear club</a></div>
+    <div class="grid grid-cols-4 gap-4">
+        @foreach ($clubs as $club)
+        <div class="bg-neon-lime w-52 h-72 m-8 static rounded-lg ">
+            <a href="{{route('clubs.show', $club->id)}}">
+                <div class="bg-white w-52 h-72 hover:-m-2 absolute rounded-lg shadow-lg transition-all duration-150 ease-out hover:ease-in ">
+                    <h1 class="m-4 text-2xl font-bold">{{$club->name}}</h1>
+                    <hr class="m-4 rounded-2xl border-t-2">
+                    <p class="m-4 text-3xl"><i class="fa-solid fa-shield-halved"></i></p>
+                </div>
+            </a>
+        </div>
+        @endforeach
+    </div>
+</div>
 @endsection
--- resources/views/clubs/show.blade.php
+++ resources/views/clubs/show.blade.php
@@ -2,21 +2,38 @@
 
 @section('title',$club->name)
 
+@section('pretitle', 'CLUB '. $club->name)
+
 @section('content')
-<h1>Club {{$club->name}}</h1>
-<p>{{$club->location}}</p>
-<a href="{{route('clubs.edit', $club)}}">Editar este club</a> 
-<br>
-<a href="{{route('clubs.teams.create', $club)}}">Añadir equipo</a>
-<ul>
-    @foreach ($club->teams as $team)
-    <li><a href="{{route('teams.show', $team->id)}}">{{$team->name}}</a></li>
-    @endforeach
-</ul>
-<form action="{{route('clubs.destroy', $club)}}" method="POST">
-@csrf
-@method('delete')
-<button type="submit">Eliminar</button>
-</form>
-<a href="{{route('clubs.index')}}">Volver a Clubs</a> 
+<div class="mt-5 font-rubik">
+    <div class="px-5 text-gray-600 hover:text-teal"><a href="{{route('clubs.index')}}">Volver a clubs</a></div>
+    <div class="mt-5 flex space-x-4">
+        <div class="flex-no-wrap space-y-3 w-1/6">
+            <div><a href="{{route('clubs.edit', $club)}}" class="p-10 inline-flex whitespace-nowrap rounded-md border border-transparent bg-teal px-4 py-2 font-rubik text-white shadow-sm hover:bg-light-teal">Editar club</a></div>
+        </div>
+        <div class="flex-no-wrap w-5/6">
+            <h1 class="mb-1 text-xl font-bold">{{$club->name}}</h1>
+            <p class="mb-3 text-gray-400">{{$club->location}}</p>
+        </div>
+    </div>
+    <div class="mt-5 flex space-x-4">
+        <div class="flex-no-wrap space-y-3 w-1/6">
+            <div><a href="{{route('clubs.teams.create', $club)}}" class="p-10 inline-flex items-center justify-center whitespace-nowrap rounded-md border border-transparent bg-neon-orange px-4 py-2 font-rubik text-white shadow-sm hover:bg-dark-orange">Nuevo equipo</a></div>
+        </div>
+        <div class="flex-col w-5/6">
+            <h2 class="mb-3 text-lg font-bold dark:text-white">TODOS LOS EQUIPOS</h2>
+            <dl class="w-1/3 text-gray-900 divide-y divide-gray-200 dark:text-white dark:divide-gray-700 w-2/3">
+                @foreach ($club->teams as $team)
+                <div class="flex justify-between">
+                    <div class="flex-col pb-3">
+                        <dt class="mt-3 font-medium md:text-lg"><a href="{{route('teams.show', $team->id)}}">{{$team->name}}</a></dt>
+                        <dd class="mb-2 text-gray-500">{{$team->category}}</dd>
+                    </div>
+                    <a class="mt-4 pb-3 font-regular text-sm" href="{{route('teams.show', $team->id)}}">Ver equipo</a>
+                </div>
+                @endforeach
+            </dl>
+        </div>
+    </div>
+</div>
 @endsection
